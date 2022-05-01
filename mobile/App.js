c3a606235152b668c28402778a887b2089d9a005
MarioMonir <mario3monir@gmail.com>
2022-05-01 12:25:43 +0200

    react native paper

--- mobile/App.js
+++ mobile/App.js
@@ -1,13 +1,17 @@
 import { Provider } from "react-redux";
+import { Provider as PaperProvider } from "react-native-paper";
 import store from "./src/Store/app.store";
 import AppNavigator from "./src/Navigators/App.navigator";
+import theme from "./src/Theme/paper.theme";
 
 // ==============================================================
 
 export default function App() {
   return (
     <Provider store={store}>
-      <AppNavigator />
+      <PaperProvider theme={theme}>
+        <AppNavigator />
+      </PaperProvider>
     </Provider>
   );
 }
